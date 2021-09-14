52dd4dd721733dffcd80951ce52cddcf598144bc
zhangjunpu <zhangjunpu050@chinasoftinc.com>
2021-09-14 17:05:10 +0800

    添加正数的补码表示

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+    size_t i;
+    for (int i = 0; i < len; ++i) {
+        printf(" %.2x", start[i]);
+        printf("\n");
+    }
+}
+
 int main() {
-    printf("Hello, World!\n");
+    short x = 12345;    //0011000000111001
+    short mx = -x;      //1100111111000111  //-12345的补码
+    show_bytes((byte_pointer) &x, sizeof(short));   // 39 30    小端
+    show_bytes((byte_pointer) &mx, sizeof(short));  // C7 CF    小端
+
+    unsigned short umx = (unsigned short )mx;
+    printf(" %d, %u\n",mx,umx);
     return 0;
 }
